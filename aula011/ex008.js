48b11e71434863d4c57a790c949a37ce26fcfb44
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-02-17 23:37:01 -0300

    query

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`Your car's speed is ${vel}mph`)
+if(vel >= 60.5) {
+    console.log(`You are fast`)
+} else {
+    console.log(`You are slow`)
+}
+console.log(`Drive carefully`)
