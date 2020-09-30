593306e11396592f2742160415ac5dee844b4ab9
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-09-30 19:56:24 -0300

    arrow

---
+++ Udemy/ECMAScript/arrow/script.js
@@ -0,0 +1,15 @@
+/* let quadrado =function(x = 0) {
+    return x*x
+} */
+
+/* let quadrado = (x) => {
+    return x*x
+} */
+
+let quadrado = (x) => x*x
+
+document.write(quadrado(16))
+
+let parOuImpar = (numero) => numero % 2 === 0 ? `par` : `impar`
+
+console.log(parOuImpar(5))
---
+++ Udemy/ECMAScript/default/script.js
@@ -0,0 +1,5 @@
+function tellAtale(dog_name = `Roger`, food, master_name) {
+    document.write(`Once Upon a time, a dog called ${dog_name}, he's loved ${food}, your master he's ${master_name}, and they lived happy forever `)
+}
+
+tellAtale(`Rex`, undefined, `Max`)
