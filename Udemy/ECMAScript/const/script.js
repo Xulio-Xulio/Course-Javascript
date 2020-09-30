820f46ab2575cb77146f20047e36989ceea67257
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-09-29 22:08:33 -0300

    let

---
+++ Udemy/ECMAScript/const/script.js
@@ -0,0 +1,10 @@
+const SERIE = `friends`
+
+function x() {
+    const SERIE = `The Walking Dead`
+    console.log(`função: ${SERIE}`)
+}
+
+x()
+
+console.log(`global: ${SERIE}`)
---
+++ Udemy/ECMAScript/var_let/script.js
@@ -0,0 +1,8 @@
+let serie = `Friends`
+
+{
+    let serie = `The Walking Dead`
+    console.log(`Dentro do bloco: ${serie}`)
+}
+
+console.log(`fora do bloco: ${serie}`)
